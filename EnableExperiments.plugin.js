0dd56aff9cf5da93f389fbab536ccba9a99fe3d9
Mustan Pathetic <patheticmustan@gmail.com>
2021-08-25 21:22:04 -0400

    oh yeah part 2 electric boogaloo

--- EnableExperiments.plugin.js
+++ EnableExperiments.plugin.js
@@ -5,6 +5,8 @@
  * @authorLink https://github.com/PatheticMustan
  * @authorId 192615247448768512
  * @version 1.0.0
+ * @updateUrl https://raw.githubusercontent.com/PatheticMustan/BetterDiscordPlugins/hackerooni/EnableExperiments.plugin.js
+ * @source https://github.com/PatheticMustan/BetterDiscordPlugins/tree/hackerooni
  */
 
 // wow plugin making is pretty fun actually
